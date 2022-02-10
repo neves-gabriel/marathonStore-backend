8842366e4f6ef0607f8517ea1886b455e2d308a1
neves-gabriel <gabrielrn.mobres@gmail.com>
2022-02-10 01:33:40 -0300

    fix: :ambulance: new controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,44 +1,21 @@
 import bcrypt from 'bcrypt';
-import signUpSchema from '../validations/signUp';
 import connection from '../database/connection.js';
 
-async function userAlredyExists(email) {
-  const existentUser = await connection.query(
-    `SELECT * FROM users WHERE email = $1;`,
-    [email]
-  );
-  if (existentUser.rowCount !== 0) return existentUser.rows;
-  return false;
-}
-
-async function createUser(req, res) {
+export default async function createUser(req, res) {
+  const newUser = req.body;
   try {
-    const newUser = req.body;
-
-    const validation = signUpSchema.validate(newUser);
-    if (validation.error) {
-      console.log(validation.error);
-      res.sendStatus(400);
+    const usersCollection = connection.collection('users');
+    if (await usersCollection.findOne({ email: newUser.email })) {
+      res.status(409).send('Um usuário com esse e-mail já está cadastrado');
       return;
     }
-
-    const { name, email, password } = newUser;
-
-    if (await userAlredyExists(email)) {
-      res.sendStatus(409);
-      return;
-    }
-
-    const passwordHash = bcrypt.hashSync(password, 10);
-    await connection.query(
-      `INSERT INTO users (name, email, password) VALUES ($1, $2, $3)`,
-      [name, email, passwordHash]
-    );
+    await connection.collection('users').insertOne({
+      ...newUser,
+      password: bcrypt.hashSync(newUser.password, 10),
+    });
     res.sendStatus(201);
   } catch (error) {
-    console.log(error.message);
-    res.sendStatus(500);
+    console.error(error);
+    res.status(500).send(error);
   }
 }
-
-export { createUser };
